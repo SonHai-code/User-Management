53b80f364a432839f1eb634aaa1b3473bf9b4fa1
SonHai-code <84950781+SonHai-code@users.noreply.github.com>
2024-02-28 10:27:49 +0700

    Create Header and Footer Component

---
+++ design-ems/src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <span>All copy rights reversed by sonhai.</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
